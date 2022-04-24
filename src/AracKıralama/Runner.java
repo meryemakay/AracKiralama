211c882a2e1e38d020a33e743a3008bba940ca01
Meryem Akay <haceryol491@gmail.com>
2022-04-24 15:04:42 +0300

    24.4.2022

---
+++ src/AracKıralama/Runner.java
@@ -0,0 +1,9 @@
+package AracKıralama;
+
+import static AracKıralama.MusteriVeArabaBilgisi.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();
+    }
+}
